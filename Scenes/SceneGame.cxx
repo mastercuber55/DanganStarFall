01ed8d929b83ffbb86c0e676622fd62d3c5c13e8
mastercuber55 <mastercuber55@gmail.com>
2025-09-05 13:28:59 +0530

    Physics Based Movement

--- Scenes/SceneGame.cxx
+++ Scenes/SceneGame.cxx
@@ -1,8 +1,8 @@
 #include "Scenes.hpp"
 
-SceneGame::SceneGame() : Player({ 0, 0, 32, 32}, "Assets/Chiaki Ship.png") {
+SceneGame::SceneGame() : Player({300, 200, 32, 32}, "Assets/Chiaki Ship.png") {
     InitPhysics();
-    SetPhysicsGravity(0, 0.5f);
+    SetPhysicsGravity(0, 0);
 
     Player.PhyInit(32);
 
@@ -12,22 +12,22 @@ void SceneGame::Update() {
 
     RunPhysicsStep();
 
-    int Speed = 16;
+    float Force = 50;
 
 	if(IsKeyDown(KEY_W)) {
-        Player.y += -Speed;
+        PhysicsAddForce(Player.Body, {0, -Force});
         Player.Rotation = 270;
     }
 	if(IsKeyDown(KEY_A)) {
-        Player.x += -Speed;
+        PhysicsAddForce(Player.Body, {-Force, 0});
         Player.Rotation = 180;
     }
 	if(IsKeyDown(KEY_S)) {
-        Player.y += +Speed;
+        PhysicsAddForce(Player.Body, {0, Force});
         Player.Rotation = 90;
     }
 	if(IsKeyDown(KEY_D)) {
-        Player.x += +Speed;
+        PhysicsAddForce(Player.Body, {Force, 0});
         Player.Rotation = 0;
     }
 }
